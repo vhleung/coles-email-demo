cc9108fb0bac7612a3d720688bb423c49391205c
VIctor Leung <victor.leung@digiflex-solutions.com>
2016-10-12 06:50:48 +1100

    initial commit

---
+++ ColesEmailDemo/src/com/ibm/demo/wcm/CustomEmailAuthoringAction.java
@@ -0,0 +1,185 @@
+package com.ibm.demo.wcm;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Locale;
+
+import javax.activation.DataHandler;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import javax.naming.InitialContext;
+
+import com.ibm.portal.ListModel;
+import com.ibm.workplace.wcm.api.Content;
+import com.ibm.workplace.wcm.api.Document;
+import com.ibm.workplace.wcm.api.DocumentId;
+import com.ibm.workplace.wcm.api.ImageComponent;
+import com.ibm.workplace.wcm.api.TemplatedDocument;
+import com.ibm.workplace.wcm.api.TextComponent;
+import com.ibm.workplace.wcm.api.extensions.authoring.ActionResult;
+import com.ibm.workplace.wcm.api.extensions.authoring.AuthoringAction;
+import com.ibm.workplace.wcm.api.extensions.authoring.FormContext;
+
+public class CustomEmailAuthoringAction implements AuthoringAction {
+
+	private static final String BASE_URL = "http://graysonline.ibmcollabcloud.com/wps/wcm/connect/Coles+Demo/Home/";
+	private static Session mailSession = null;
+
+	@Override
+	public String getDescription(Locale locale) {
+		return "Send as Email";
+	}
+
+	@Override
+	public ListModel<Locale> getLocales() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return "Send as Email";
+	}
+
+	@Override
+	public ActionResult execute(FormContext formContext) {
+		Document document = formContext.document();
+		Content content = (Content) document;
+		
+		System.out.println("Send_as_Email button clicked for " + document.getId());
+		
+		// reference: http://blog.smartbear.com/how-to/how-to-send-email-with-embedded-images-using-java/
+
+		try {
+			String to = ((TextComponent) content.getComponentByReference("To")).getText();
+			String subject = ((TextComponent) content.getComponentByReference("Subject")).getText();
+			byte[] bannerImage = ((ImageComponent) content.getComponentByReference("Banner Image")).getImage();
+			String bannerImageFilename = ((ImageComponent) content.getComponentByReference("Banner Image")).getImageFileName();
+			byte[] mainImage = ((ImageComponent) content.getComponentByReference("Main Image")).getImage();
+			String mainImageFilename = ((ImageComponent) content.getComponentByReference("Main Image")).getImageFileName();
+			byte[] sidebarImage = ((ImageComponent) content.getComponentByReference("Sidebar Image")).getImage();
+			String sidebarImageFilename = ((ImageComponent) content.getComponentByReference("Sidebar Image")).getImageFileName();
+			
+			HttpURLConnection connection = null;
+		    URL url = new URL(BASE_URL + content.getName());
+		    connection = (HttpURLConnection) url.openConnection();
+		    connection.setRequestMethod("GET");
+		    connection.setUseCaches(false);
+		    connection.setDoOutput(true);
+		    
+		    InputStream is = connection.getInputStream();
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		    StringBuilder response = new StringBuilder(); 
+		    String line;
+		    while ((line = rd.readLine()) != null) {
+		      response.append(line);
+		      response.append('\r');
+		    }
+		    rd.close();
+			  
+			MimeMessage msg = new MimeMessage(getMailSession());
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));	
+			msg.setFrom(new InternetAddress("vicleung@au1.ibm.com"));
+			msg.setSubject(subject);
+			
+			MimeMultipart multiPart = new MimeMultipart();
+			multiPart.setSubType("related");
+			msg.setContent(multiPart);
+					
+			MimeBodyPart mainPart = new MimeBodyPart();
+			// mainPart.setContent("<!DOCTYPE html><html><body><b>hello world</b> <img src='cid:bannerimage'> <img src='cid:sidebaroneimage'>  <img src='cid:sidebartwoimage'>  this is normal text <p> yeah yeah </body></html>", "text/html");
+			mainPart.setContent(response.toString(), "text/html");
+			multiPart.addBodyPart(mainPart);
+			
+			MimeBodyPart bannerImagePart = new MimeBodyPart();
+			ByteArrayDataSource bannerImageDs = new ByteArrayDataSource(bannerImage, "image/jpeg");
+			bannerImagePart.setDataHandler(new DataHandler(bannerImageDs));
+			bannerImagePart.setFileName(bannerImageFilename);
+			bannerImagePart.setContentID("<bannerimage>");
+			bannerImagePart.setDisposition(MimeBodyPart.INLINE);
+			multiPart.addBodyPart(bannerImagePart);
+			
+			MimeBodyPart mainImagePart = new MimeBodyPart();
+			ByteArrayDataSource sidebarOneImageDs = new ByteArrayDataSource(mainImage, "image/jpeg");
+			mainImagePart.setDataHandler(new DataHandler(sidebarOneImageDs));
+			mainImagePart.setFileName(mainImageFilename);
+			mainImagePart.setContentID("<mainimage>");
+			mainImagePart.setDisposition(MimeBodyPart.INLINE);
+			multiPart.addBodyPart(mainImagePart);
+			
+			MimeBodyPart sidebarImagePart = new MimeBodyPart();
+			ByteArrayDataSource sidebarTwoImageDs = new ByteArrayDataSource(sidebarImage, "image/jpeg");
+			sidebarImagePart.setDataHandler(new DataHandler(sidebarTwoImageDs));
+			sidebarImagePart.setFileName(sidebarImageFilename);
+			sidebarImagePart.setContentID("<sidebarimage>");
+			sidebarImagePart.setDisposition(MimeBodyPart.INLINE);
+			multiPart.addBodyPart(sidebarImagePart);
+			
+			Transport.send(msg);
+			
+			System.out.println("Sent: Send_as_Email button for " + document.getId());
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// do nothing
+		}
+		
+		System.out.println("Done: Send_as_Email button for " + document.getId());
+
+		return null;
+	}
+
+	@Override
+	public boolean isValidForForm(FormContext formContext) {
+		Document document = formContext.document();
+
+		if (document instanceof TemplatedDocument) {
+			Content content = (Content) document;
+
+			try {
+				DocumentId templateId = content.getAuthoringTemplateID();
+
+				Document template = content.getSourceWorkspace().getById(templateId);
+
+				if (template.getName().contains("Email")) {
+					return true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				// do nothing
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public int ordinal() {
+		return 0;
+	}
+
+	public static Session getMailSession() {
+		if (mailSession == null) {
+			try {
+				InitialContext ctx = new InitialContext();
+				mailSession = (Session) ctx.lookup("mail/localmail");
+			} catch (Exception e) {
+				e.printStackTrace();
+				// do nothing
+			}
+		}
+
+		return mailSession;
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# coles-email-demo
